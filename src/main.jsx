8bf421d974469d9d2db1cfd078344c9f4e2da865
Mathilde <mathilde@Mathildes-MacBook-Pro.local>
2024-03-11 18:37:18 +0100

    still trying to setup github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
 import './index.css';
-import { HashRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <App />,
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
